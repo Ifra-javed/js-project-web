59b8797b6beba75cfcebcd53d7410c010a2bfb7a
Ifra-javed <158821818+Ifra-javed@users.noreply.github.com>
2024-03-14 11:05:28 +0000

    datatypes done

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -1,4 +1,39 @@
 "use strict"; //treat all js code as newer version
 
 //alert("Assalama Alikum")//we are using node js not browser
+//readability should be high
+
+let name = "ifra" 
+let age = 18
+let isloggedin = false
+
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value 
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "ifra");//string
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
