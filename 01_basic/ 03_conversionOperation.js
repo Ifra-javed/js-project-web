f9a66d6447301f7c4dbdd7267e29b49de21a41ae
Ifra-javed <158821818+Ifra-javed@users.noreply.github.com>
2024-03-16 18:18:07 +0000

    operation done

--- 01_basic/ 03_conversionOperation.js
+++ 01_basic/ 03_conversionOperation.js
@@ -22,3 +22,37 @@ let someNumber = 33
 let stringNumber = string(someNumber)
 console.log(stringNumber);
 
+//...............operation...................
+let value = 3
+let negValue = -value
+//console.log(negValue );
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+
+let str1 = "ifra"
+let str2 = "javed"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" +2 + 2);//it will give 122
+console.log((1+ 2 + "2"));//it will give 32
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
