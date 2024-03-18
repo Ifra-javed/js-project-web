8c425d4278e98b555b69c8568947eb0d4c218450
Ifra-javed <158821818+Ifra-javed@users.noreply.github.com>
2024-03-18 17:35:33 +0000

    conversion operation done

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,9 +1,9 @@
-/*let  score = "333abc"
+let  score = "333abc"
 console.log(typeof score);
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
-console.log(valueInNumber);*/
+console.log(valueInNumber);
 
 //"33" = 33
 //"33abc" = NaN
@@ -25,14 +25,14 @@ console.log(stringNumber);
 //...............operation...................
 let value = 3
 let negValue = -value
-//console.log(negValue );
+console.log(negValue );
 
-//console.log(2+2);
-//console.log(2-2);
-//console.log(2*2);
-//console.log(2**2);
-//console.log(2/2);
-//console.log(2%2);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
 
 
 let str1 = "ifra"
@@ -45,8 +45,8 @@ console.log(1 + "2");
 console.log("1" +2 + 2);//it will give 122
 console.log((1+ 2 + "2"));//it will give 32
 
-console.log(+true);
-console.log(+"");
+console.log(+true);  //true will print 1
+console.log(+""); //it will print 0
 
 let num1,num2,num3
 num1 = num2 = num3 = 2 + 2
@@ -55,4 +55,3 @@ let gameCounter = 100
 gameCounter++;
 ++gameCounter;
 console.log(gameCounter);
-
