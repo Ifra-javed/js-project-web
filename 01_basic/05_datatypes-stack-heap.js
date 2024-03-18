be82f2c104bbade51655a8bf03170d2aa6b18317
Ifra-javed <158821818+Ifra-javed@users.noreply.github.com>
2024-03-18 17:55:08 +0000

    stack heap done and datatypes

--- 01_basic/05_datatypes-stack-heap.js
+++ 01_basic/05_datatypes-stack-heap.js
@@ -13,15 +13,15 @@
 
 // ............examples of primitive...........
 
-//const score = 33
-// const scoreValue = 100.4
-// const outsideTemp = null
-// const isLoggedIn = false
-// let userEmail;
-// const id = Symbol("246")
-// const anotherId = Symbol("246")
-// console.log(id ===anotherId);
-// const bigNumber = 38847895498947949797499849
+const score = 33
+const scoreValue = 100.4
+const outsideTemp = null
+const isLoggedIn = false
+let userEmail;
+const id = Symbol("246")
+const anotherId = Symbol("246")
+console.log(id ===anotherId);
+const bigNumber = 38847895498947949797499849
 
 
 
@@ -54,29 +54,29 @@ let myObj = {
 
 // .......To find datatypes.......
 
-// console.log(typeof bigNumber);
-// console.log(typeof outsideTemp);
-// console.log(typeof scoreValue);
-// console.log(typeof myFun);
-// console.log(typeof anotherId);
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof myFun);
+console.log(typeof anotherId);
 
 
 
 // ...........stack and heap memory...............
 
 // stack(primitive values).........heap(non primitive values)
-
 // stack main values ki copy  or  heap main reference ha
+
 
 // .....example of stack.....
 
 //if we change tha value from second variable it will be change in second not in first variable
 
-// let myEmail  = "ifra@gmail.com"
-// let anotherEmail = myEmail
-// anotherEmail = "saduuu@gmail.com"
-// console.log(myEmail);
-// console.log(anotherEmail);
+ let myEmail  = "ifra@gmail.com"
+ let anotherEmail = myEmail
+ anotherEmail = "saduuu@gmail.com"
+ console.log(myEmail);
+console.log(anotherEmail);
 
 // ........example of heap...........
 //if we change tha value from second variable it will also be change in first variable
