555a2b96d41514ca5e51d6d20171e498ff30c3c8
Ifra-javed <158821818+Ifra-javed@users.noreply.github.com>
2024-03-20 08:12:24 +0000

    number math done

---
+++ 01_basic/07_numbers-and-math.js
@@ -0,0 +1,19 @@
+//''''''''''''''''''''Maths''''''''''''''''''''''''''
+
+console.log(Math);
+console.log(Math.abs(-4));//to convert negative value into positive
+console.log(Math.round(8.6));//to remove the points value....mostly used
+console.log(Math.ceil(8.2));//it will give highest value
+console.log(Math.floor(8.2));//it will give lowest value
+console.log(Math.sqrt(25));
+console.log(Math.min(4,6,8,2));//it will give lowest value in array
+console.log(Math.max(4,6,8,9,4));//it will give highest value in array 
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log((Math.random() * (max - min + 1))+ min)// it will give high value from 10
+console.log((Math.random() * (max - min + 1))+ max)//it will give high value from 20
