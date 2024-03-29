aaa29ee502bc05cedaf3f7b2c084e692bd6a21d5
Ifra-javed <158821818+Ifra-javed@users.noreply.github.com>
2024-03-29 06:51:46 +0000

    scope and mini hoiisting done

--- 01_basic/02_basic/12_global-and-local-scope.js
+++ 01_basic/02_basic/12_global-and-local-scope.js
@@ -16,3 +16,44 @@ if(true){
 console.log(a)
 console.log(b)
 console.log(c)
+
+//.........nested scope..........
+
+
+function one(){
+    const username = "ifra"
+
+    function two(){
+        const website  = "javascript"
+        console.log(username);
+    }
+     //console.log(website);
+    two()
+}
+//one()
+
+//.........if else......... 
+
+if(true){
+    const username = "ifra"
+    if(username === "ifra"){
+        const website = " javascript"
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+//................interesting.........mini hoiisting........
+
+//this one can acces addone before initialization 
+console.log(addone(6));
+function addone(num){
+    return num + 1
+}
+
+console.log(addtwo(6)); //this one can not acces addtwo before initialization it will through error  
+const addtwo = function(num){ //this  one sometime called expression..like variable
+    return num +2
+}
+
