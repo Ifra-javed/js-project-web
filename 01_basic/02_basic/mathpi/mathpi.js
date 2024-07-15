de140880814c219faad49b52d7fd8080bb44d066
Ifra-javed <158821818+Ifra-javed@users.noreply.github.com>
2024-07-15 10:29:03 +0000

    mathpi

---
+++ 01_basic/02_basic/mathpi/mathpi.js
@@ -0,0 +1,38 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descriptor);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: "tea",
+    price: 240,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+
+}
+
+console.log(chai);
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, "name", {
+    // writable:false,
+    enumerable:false
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value !== "function"){
+
+    console.log(`${key } : ${value}`);
+
+    }
+
+}
